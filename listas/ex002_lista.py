e94cfb1988ca5c6f28e90ee6083da9e41fc70fb3
João Rodrigues <149261135+JohnVrNas@users.noreply.github.com>
2024-03-10 21:54:01 -0300

    Delete listas directory

--- listas/ex002_lista.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system('cls')
-num = []
-for i in range(6):
-    num.append(int(input(f'Digite o {i+1}º número: '))) 
-    
-for i in range (6):
-    print(f'{i} => {num[i]}')
-    
--- listas/ex003_lista.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system('cls')
-print('Média de números')
-num = []
-for c in range(4):
-    num.append(int(input(f'Digite o {c+1}° números: ')))
-soma = (sum(num))/4
-print(f'A média dos números é de {soma:.2f}')
