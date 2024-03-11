9e5fe64f76077a5b1692a411baf4efc72872763c
João Rodrigues <149261135+JohnVrNas@users.noreply.github.com>
2024-03-10 21:53:32 -0300

    Delete Aula 8 directory

--- Aula 8/ex05aula8.py
+++
@@ -1,7 +0,0 @@
-numeros = []
-for i in range(20):
-    numero = float(input(f"Digite o {i + 1}º número: "))
-    numeros.append(numero)
-for numero in numeros:
-    metade = numero / 2
-    print(f"A metade de {numero} é {metade}")
--- Aula 8/ex07aula8.py
+++
@@ -1,7 +0,0 @@
-print('Somatória de número')
-s = 0
-n =  int(input('Digite o número máximo: '))
-for i in range(0, n+1):
-    s += i
-    print(i)
-print(s)
